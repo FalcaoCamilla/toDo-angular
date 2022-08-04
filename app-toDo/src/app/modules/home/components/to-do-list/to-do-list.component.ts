4f1f24036d8544493c9a37bd2dbc09827f861043
FalcaoCamilla <millafalcao.acad@gmail.com>
2022-08-04 11:13:12 -0300

    taskList array and length with ngClass and ngIf

--- app-toDo/src/app/modules/home/components/to-do-list/to-do-list.component.ts
+++ app-toDo/src/app/modules/home/components/to-do-list/to-do-list.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+//Interface
+import { TaskList } from '../../model/task-list';
+
 @Component({
   selector: 'app-to-do-list',
   templateUrl: './to-do-list.component.html',
   styleUrls: ['./to-do-list.component.scss']
 })
 export class ToDoListComponent implements OnInit {
+
+  public taskList: Array<TaskList> = [
+
+  ];
 
   constructor() { }
 
--- app-toDo/src/app/modules/home/home.module.ts
+++ app-toDo/src/app/modules/home/home.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 //Components
 import { HeaderComponent } from './components/header/header.component';
@@ -11,7 +12,6 @@ import { ToDoListComponent } from './components/to-do-list/to-do-list.component'
 import { HomeComponent } from './pages/home/home.component';
 
 
-
 @NgModule({
   declarations: [
     HeaderComponent,
@@ -21,7 +21,8 @@ import { HomeComponent } from './pages/home/home.component';
     HomeComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ]
 })
 export class HomeModule { }
