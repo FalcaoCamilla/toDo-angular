156f75cc32b14e7aeaf7c4912fc0f617012a20e8
FalcaoCamilla <millafalcao.acad@gmail.com>
2022-08-04 09:46:19 -0300

    initial config

---
+++ app-toDo/src/app/modules/home/home.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+//Components
+import { HeaderComponent } from './components/header/header.component';
+import { ToDoButtonDeleteAllComponent } from './components/to-do-button-delete-all/to-do-button-delete-all.component';
+import { ToDoInputAddItensComponent } from './components/to-do-input-add-itens/to-do-input-add-itens.component';
+import { ToDoListComponent } from './components/to-do-list/to-do-list.component';
+
+//Pages
+import { HomeComponent } from './pages/home/home.component';
+
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    ToDoButtonDeleteAllComponent,
+    ToDoInputAddItensComponent,
+    ToDoListComponent,
+    HomeComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class HomeModule { }
